d984b513d9366971bf8c61c221a9073f2e455d31
bo-dun-1 <pdun2008@gmail.com>
2023-06-10 21:42:52 -0700

    initialize cartel app

---
+++ src/FeatheryConfig.js
@@ -0,0 +1,4 @@
+// You can use this existing form as quick start, but you will need to swap the
+// credentials to your own form if you'd like to customize it!
+export const FEATHERY_SDK_KEY = "e78de5c8-da25-43dc-9e81-4a592c06dce1";
+export const EMBED_FORM_NAME = "BC New Account Onboarding Test";
