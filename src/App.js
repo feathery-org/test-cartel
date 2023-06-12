c3fa8a0c1ed86ee9765eb708357d3e874337c487
bo-dun-1 <pdun2008@gmail.com>
2023-06-12 12:15:32 -0700

    add div wrapper

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { Form } from "@feathery/react";
 import { EMBED_FORM_NAME } from "./FeatheryConfig";
 
 function App() {
-  return <Form formName={EMBED_FORM_NAME} />;
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Form formName={EMBED_FORM_NAME} />
+    </div>
+  );
 }
 
 export default App;
